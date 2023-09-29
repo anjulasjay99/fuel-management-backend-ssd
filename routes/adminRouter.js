19142765c872430842ff50af762788a701eb1ec7
rishitha98 <rishithad98@gmail.com>
2023-09-29 22:40:18 +0530

    implement rate limit

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,9 +1,31 @@
 const router = require("express").Router();
 const Admin = require("../models/Admin");
+const rateLimit = require("express-rate-limit");
 
-router.route("/login").post(async (req, res) => {
+
+const apiLimiter = rateLimit({
+	windowMs: 1 * 60 * 1000, // 1 minutes
+	limit: 2, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: 'draft-7', // Set `RateLimit` and `RateLimit-Policy` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  handler: (req, res, next) => {
+    // Log a message when the rate limit is exceeded
+    console.log("Dos attack is encountered")
+    console.log("Rate limiter exceeded for IP:", req.ip);
+    res.status(429).json({
+      status: false,
+      msg: "Too many login attempts. Please try again after one minute.",
+    });
+    //next(); // Continue with the route handler
+  },
+	// store: ... , // Use an external store for more precise rate limiting
+})
+
+
+
+router.route("/login").post( apiLimiter,async (req, res) => {
   const { email, password } = req.body;
-
+  
   await Admin.findOne({ email })
     .then((data) => {
       if (data.password === password) {
@@ -16,5 +38,16 @@ router.route("/login").post(async (req, res) => {
       res.status(400).json({ status: false, msg: "Incorrect Credentials" });
     });
 });
+
+// // Handle the rate limiter error message
+// router.use((req, res, next) => {
+//   if (req.rateLimit && req.rateLimit.message) {
+//     console.log(req.rateLimit.message)
+//     res.status(429).json({ status: false, msg: req.rateLimit.message });
+//   } else {
+//     next();
+//   }
+// });
+
 
 module.exports = router;
