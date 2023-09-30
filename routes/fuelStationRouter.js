e901e27bc61b18aea212096f5c41a9eb3acf37ba
anjulasjay99 <anjulasjay@gmail.com>
2023-09-30 06:47:27 +0530

    Added JWTs

--- routes/fuelStationRouter.js
+++ routes/fuelStationRouter.js
@@ -1,5 +1,17 @@
 const router = require("express").Router();
 let FuelStation = require("../models/FuelStation");
+const jwt = require("jsonwebtoken");
+
+//check if user is authorized
+const auth = (token) => {
+  try {
+    jwt.verify(token, "fsToken");
+    return true;
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+};
 
 //check if email already exists
 router.route("/checkEmail").post(async (req, res) => {
@@ -90,24 +102,11 @@ router.route("/register").post(async (req, res) => {
 
 //update fuel station
 router.route("/updateInfo").put(async (req, res) => {
-  const {
-    stationId,
-    stationName,
-    type,
-    address,
-    city,
-    province,
-    zipCode,
-    contactNo,
-    ownerName,
-    ownerNic,
-    ownerContactNo,
-    ownerEmail,
-  } = req.body;
+  const token = req.header("x-access-token");
 
-  await FuelStation.findOneAndUpdate(
-    { stationId },
-    {
+  if (auth(token)) {
+    const {
+      stationId,
       stationName,
       type,
       address,
@@ -119,63 +118,97 @@ router.route("/updateInfo").put(async (req, res) => {
       ownerNic,
       ownerContactNo,
       ownerEmail,
-    }
-  )
-    .then(() => {
-      res.status(200).json({ status: true, msg: "Success" });
-    })
-    .catch((err) => {
-      res.status(400).json({ status: false, error: err });
-    });
+    } = req.body;
+
+    await FuelStation.findOneAndUpdate(
+      { stationId },
+      {
+        stationName,
+        type,
+        address,
+        city,
+        province,
+        zipCode,
+        contactNo,
+        ownerName,
+        ownerNic,
+        ownerContactNo,
+        ownerEmail,
+      }
+    )
+      .then(() => {
+        res.status(200).json({ status: true, msg: "Success" });
+      })
+      .catch((err) => {
+        res.status(400).json({ status: false, error: err });
+      });
+  } else {
+    res.status(400).json("Authentication Failed!");
+  }
 });
 
 //update fuel station
 router.route("/updateEmail").put(async (req, res) => {
   const { stationId, email } = req.body;
+  const token = req.header("x-access-token");
 
-  await FuelStation.findOneAndUpdate(
-    { stationId },
-    {
-      email,
-    }
-  )
-    .then(() => {
-      res.status(200).json({ status: true, msg: "Success" });
-    })
-    .catch((err) => {
-      res.status(400).json({ status: false, error: err });
-    });
+  if (auth(token)) {
+    await FuelStation.findOneAndUpdate(
+      { stationId },
+      {
+        email,
+      }
+    )
+      .then(() => {
+        res.status(200).json({ status: true, msg: "Success" });
+      })
+      .catch((err) => {
+        res.status(400).json({ status: false, error: err });
+      });
+  } else {
+    res.status(400).json("Authentication Failed!");
+  }
 });
 
 //update fuel station
 router.route("/updatePassword").put(async (req, res) => {
   const { stationId, password } = req.body;
+  const token = req.header("x-access-token");
 
-  await FuelStation.findOneAndUpdate(
-    { stationId },
-    {
-      password,
-    }
-  )
-    .then(() => {
-      res.status(200).json({ status: true, msg: "Success" });
-    })
-    .catch((err) => {
-      res.status(400).json({ status: false, error: err });
-    });
+  if (auth(token)) {
+    await FuelStation.findOneAndUpdate(
+      { stationId },
+      {
+        password,
+      }
+    )
+      .then(() => {
+        res.status(200).json({ status: true, msg: "Success" });
+      })
+      .catch((err) => {
+        res.status(400).json({ status: false, error: err });
+      });
+  } else {
+    res.status(400).json("Authentication Failed!");
+  }
 });
 
 //delete fuel station
 router.route("/").delete(async (req, res) => {
   const { stationId } = req.body;
+  const token = req.header("x-access-token");
 
-  await FuelStation.findOneAndRemove({ stationId })
-    .then((data) => {
-      res.status(200).json({ status: true, msg: "Success" });
-    })
-    .catch((err) => {
-      res.status(400).json({ status: false, error: err });
-    });
+  if (auth(token)) {
+    await FuelStation.findOneAndRemove({ stationId })
+      .then((data) => {
+        res.status(200).json({ status: true, msg: "Success" });
+      })
+      .catch((err) => {
+        res.status(400).json({ status: false, error: err });
+      });
+  } else {
+    res.status(400).json("Authentication Failed!");
+  }
 });
 
 router.route("/login").post(async (req, res) => {
@@ -183,7 +216,10 @@ router.route("/login").post(async (req, res) => {
   await FuelStation.findOne({ email })
     .then((data) => {
       if (data.password === password) {
-        res.status(200).json({ status: true, msg: "Success", userData: data });
+        const token = jwt.sign({ ...data }, "fsToken");
+        res
+          .status(200)
+          .json({ status: true, msg: "Success", userData: data, token });
       } else {
         res.status(400).json({ status: false, msg: "Incorrect Credentials" });
       }
